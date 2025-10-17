10d34a73066e7bfd0710ee64d5fd8ddffd1b2ec7
Bohdan Naumenko <131257541+setygur@users.noreply.github.com>
2025-10-17 18:28:44 +0200

    GameFrame code added

--- src/base/Main.java
+++ src/base/Main.java
@@ -1,4 +1,10 @@
 package base;
 
+import base.UI.GameFrame;
+
 public class Main {
+
+    public static void main(String[] args) {
+        GameFrame gameFrame = new GameFrame();
+    }
 }
--- src/base/UI/GameFrame.java
+++ src/base/UI/GameFrame.java
@@ -1,6 +1,27 @@
 package base.UI;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class GameFrame extends Frame {
+
+    private GamePanel gamePanel;
+
+    public GameFrame() {
+        gamePanel = new GamePanel();
+
+        this.setLayout(new BorderLayout());
+        this.add(gamePanel);
+        this.setSize(500, 500);
+        this.setVisible(true);
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+    }
 }
